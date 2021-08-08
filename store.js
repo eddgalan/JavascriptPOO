d63c823397bcb433e364c852c5c16440bbe6d36e
Edson Galan <edd.galan@hotmail.com>
2021-08-08 18:18:31 -0500

    Herencia con prototipos - Uso de la función bind() para solucionar 'errores'.

--- store.js
+++ store.js
@@ -22,9 +22,24 @@
     console.log('Store #'+ this.storeId + ' tiene pedidos pendientes');
     // Obtiene todos los emails de los pedidos
     var customerEmails = Object.keys(this.db.getAll());
-    for( let email of customerEmails ){
+    /*  El siguiente código manda un error por la variable 'this' el valor de 'this'
+    es relativo depende donde se encuentre  */
+    // customerEmails.forEach(function(email){
+    //   console.log(this.db.get(email));
+    // });
+
+    /* Tenemos dos métodos para solucionar este error, el primero sería el siguiente: */
+    // var self = this;
+    // customerEmails.forEach(function(email){
+    //   console.log(self.db.get(email));
+    // });
+
+    /* La otra solución sería la siguiente: */
+    customerEmails.forEach(function(email){
       console.log(this.db.get(email));
-    }
+    }.bind(this));
+    /* El método bind() crea una nueva función, que cuando es llamada,
+    asigna a su operador  this el valor entregado */
   };
 
   App.Store = Store;
