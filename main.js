b64d4d5a4653b7e02a4544c3424137aa0cce0e3f
Edson Galan <edd.galan@hotmail.com>
2021-08-08 18:06:32 -0500

    Palabra reservada this - Agrega más métodos para store.js - Creación de una store y un pedido

--- main.js
+++ main.js
@@ -6,19 +6,9 @@
   var Order = App.Order;
 
   var datastore = new DataStore();
-  // Agregar propiedades al objeto
-  datastore.add('Nombre', 'Alpha Store');
-  datastore.add('Dirección', 'CDMX');
-  datastore.add('CodigoPostal', '56260');
-  // Muestra el objeto en la consola del navegador
-  console.log(datastore);
-  // Muestra una propiedad del objeto haciendo uso de la función 'get' que definimos en el datastore.js
-  console.log(datastore.get('Nombre'));
-  // Muestra todos los datos del objeto datastore haciendo uso de la función 'getAll'
-  console.log(datastore.getAll());
-  // Remueve una propiedad del objeto datastore haciendo uso de la función 'remove'
-  datastore.remove('CodigoPostal');
-  // Muestra el objeto datastore (Ya no muestra la propiedad 'CP' porque la removimos)
-  console.log(datastore.getAll());
-  console.log(window);
+  var store = new Store('Alpha-00', datastore);
+  var order = new Order('email@correo.com', 'PS4', 8999);
+
+  store.createOrder(order);
+  store.printOrders();
 }(window));
--- store.js
+++ store.js
@@ -8,6 +8,25 @@
     this.db = db; // Composición
   }
 
+  Store.prototype.createOrder = function(order){
+    console.log('Agregando pedido para: '+ order.email);
+    this.db.add(order.email, order);
+  };
+  // Entrega un pedido
+  Store.prototype.deliverOrder = function(email){
+    console.log('Entrega de pedido para: '+ email);
+    this.db.remove(email);
+  };
+  // Muestra todos los pedidos
+  Store.prototype.printOrders = function(){
+    console.log('Store #'+ this.storeId + ' tiene pedidos pendientes');
+    // Obtiene todos los emails de los pedidos
+    var customerEmails = Object.keys(this.db.getAll());
+    for( let email of customerEmails ){
+      console.log(this.db.get(email));
+    }
+  };
+
   App.Store = Store;
   window.App = App;
 }());
